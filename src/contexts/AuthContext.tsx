24b67d084a7c6ffcf1bb52b1fffeab82bdcdc6e3
Felipe Abreu <felipe.claro654@gmail.com>
2022-06-14 17:30:30 -0300

    authenticated user info and todolist private

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext } from 'react'
+import Router from 'next/router'
+import { createContext, useState } from 'react'
 import { api } from 'services/auth'
+
+type User = {
+  email: string
+  permissions: string[]
+  roles: string[]
+}
 
 type SignInCredentials = {
   email: string
@@ -9,6 +16,7 @@ type SignInCredentials = {
 type AuthContextData = {
   signIn(credencials: SignInCredentials): Promise<void>
   isAuthenticated: boolean
+  user: User
 }
 
 export const AuthContext = createContext({} as AuthContextData)
@@ -18,20 +26,30 @@ type AuthProviderProps = {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false
-
+  const [user, setUser] = useState<User>({
+    email: '',
+    permissions: [],
+    roles: [],
+  })
+  const isAuthenticated = !!user
   async function signIn({ email, password }: SignInCredentials) {
-    console.log('blaaa')
     try {
-      const response = await api.post('sessions', { email, password })
-      console.log(response)
+      const {
+        data: { permissions, roles },
+      } = await api.post('sessions', {
+        email,
+        password,
+      })
+
+      setUser({ email, permissions, roles })
+      Router.push('/todolist')
     } catch (err) {
       console.log(err)
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
